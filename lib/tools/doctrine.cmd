457abf96f936e8c2d14b36082965230e46084d65
Terry SoRelle <tls@2quakers.net>
2015-04-16 16:16:27 -0500

    updated less processing in scym module

--- lib/tools/doctrine.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-SET TOPS_TARGET=D:\Projects\websites\scym.org\next\public_html
-cd D:\Projects\websites\tops\tops.test
-SET BIN_TARGET="D:\Projects\websites\tops\tops.test\vendor\doctrine\orm\bin\doctrine.php"
-php "%BIN_TARGET%" %*
--- lib/tools/php/hashpwd.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Terry
- * Date: 11/30/2014
- * Time: 8:31 AM
- */
