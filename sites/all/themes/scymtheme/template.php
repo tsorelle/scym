cb68f664d78a7e65a5b380c7be75464fe4932156
Terry SoRelle <tls@2quakers.net>
2015-04-17 09:03:51 -0500

    theme fixes

---
+++ sites/all/themes/scymtheme/template.php
@@ -0,0 +1,24 @@
+<?php
+
+use Drupal\tops\Mvvm\TViewModel;
+
+
+/**
+ * Overrides theme_status_messages
+ *
+ * Inserts a KnockoutJS component element for ViewModel pages
+ *
+ * @param $variables
+ * @return string|void
+ */
+function topsy_status_messages($variables) {
+    $vmPath = TViewModel::getVmPath();
+    $result = bootstrap_status_messages($variables);
+    if (!empty($vmPath)) {
+        $result = "$result\n<messages-component></messages-component>";
+    }
+    return $result;
+}
+
+
+
